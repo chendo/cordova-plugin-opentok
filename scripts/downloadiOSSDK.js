5149ae55ec4b1a9412aeb5ec4789a4dc10e567c1
Jack Chen (chendo) <github@chen.do>
2016-07-07 15:33:21 +1000

    Turns out there's a 2.8.3

--- scripts/downloadiOSSDK.js
+++ scripts/downloadiOSSDK.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var IosSDKVersion = "OpenTok-iOS-2.7.1-beta.1";
+    var IosSDKVersion = "OpenTok-iOS-2.8.3";
     var downloadFile = require('./downloadFile.js'),
         exec = require('./exec/exec.js'),
         Q = context.requireCordovaModule('q'),
         deferral = new Q.defer();
-    console.log('Downloading OpenTok iOS SDK');
+    console.log('Downloading OpenTok iOS SDK: ' + IosSDKVersion);
     downloadFile('https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/' + IosSDKVersion + '.tar.bz2',
         './' + IosSDKVersion + '.tar.bz2', function (err) {
             if (!err) {
